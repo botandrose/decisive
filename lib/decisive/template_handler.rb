a2e8cf94f5de210b94e21e8a6dfcb9108706b943
Micah Geisel <micah@botandrose.com>
2018-09-19 16:54:20 -0700

    refactor to prevent instance variables polluting the ActionView context.

--- lib/decisive/template_handler.rb
+++ lib/decisive/template_handler.rb
@@ -11,48 +11,52 @@ module Decisive
     def self.call template
       <<~RUBY
         extend Decisive::DSL
-        #{template.source}
-        response.headers["Content-Disposition"] = %(attachment; filename="\#{@filename}")
-        rows
-          .map { |rows| rows.map(&:to_s) }
-          .map(&:to_csv)
-          .join
+        context = (#{template.source})
+        response.headers["Content-Disposition"] = %(attachment; filename="\#{context.filename}")
+        context.to_csv
       RUBY
     end
   end
 
   module DSL
-    def self.extended object
-      object.instance_variable_set :@columns, []
-    end
-    
     def csv records, filename:, &block
-      @filename = filename
-      @records = records
-      instance_eval &block
+      Context.new([], records, filename).tap do |context|
+        context.instance_eval &block
+      end
     end
+  end
 
+  class Context < Struct.new(:columns, :records, :filename)
     class Column < Struct.new(:field, :label, :block); end
 
     def column field, label: field.to_s.humanize, &block
       block ||= ->(record) { record.send(field) }
-      @columns << Column.new(field, label, block)
+      columns << Column.new(field, label, block)
     end
 
-    def header
-      @columns.map(&:label)
+    def to_csv
+      rows
+        .map { |rows| rows.map(&:to_s) }
+        .map(&:to_csv)
+        .join
     end
 
-    def body
-      @records.map do |record|
-        @columns.map do |column|
-          column.block.call(record)
-        end
-      end
-    end
+    private
 
     def rows
       [header] + body
+    end
+
+    def header
+      columns.map(&:label)
+    end
+
+    def body
+      records.map do |record|
+        columns.map do |column|
+          column.block.call(record)
+        end
+      end
     end
   end
 end
