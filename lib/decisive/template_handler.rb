a0846bf493f97ea117c4bfee85dcf65778dea7e2
Micah Geisel <micah@botandrose.com>
2019-06-05 16:33:07 -0600

    introduce stream_csv method for streaming when columns are defined up-front.

--- lib/decisive/template_handler.rb
+++ lib/decisive/template_handler.rb
@@ -24,6 +24,7 @@ module Decisive
           ensure
             response.stream.close
           end
+          ""
         else
           context.to_csv
         end
@@ -35,14 +36,13 @@ module Decisive
     def csv records, filename:, &block
       Context.new(records, filename, block)
     end
+
+    def stream_csv records, filename:, &block
+      StreamContext.new([], records, filename, &block)
+    end
   end
 
   class Context < Struct.new(:records, :filename, :block)
-    def each &block
-      block.call header
-      body.each &block.method(:call)
-    end
-
     def to_csv
       (header + body).map(&:to_csv).join
     end
@@ -68,27 +68,59 @@ module Decisive
         Row.new(record, block).to_hash
       end
     end
+
+    class Row < Struct.new(:record, :block)
+      def to_hash
+        @hash = {}
+        instance_exec record, &block
+        @hash
+      end
+
+      private
+
+      def column key, value=nil, &block
+        @hash[key] = if block_given?
+          block.call(record)
+        elsif value.is_a?(Symbol)
+          record.send(value)
+        elsif value.nil?
+          record.send(key.parameterize.underscore.to_sym)
+        else
+          value
+        end.to_s
+      end
+    end
   end
 
-  class Row < Struct.new(:record, :block)
-    def to_hash
-      @hash = {}
-      instance_exec record, &block
-      @hash
+  class StreamContext < Struct.new(:columns, :records, :filename)
+    class Column < Struct.new(:label, :block); end
+
+    def initialize *args, &block
+      super
+      instance_eval &block
+    end
+
+    def column label, value=nil, &block # field, label: field.to_s.humanize, &block
+      value ||= label.parameterize.underscore.to_sym
+      block ||= ->(record) { record.send(value) }
+      columns << Column.new(label, block)
+    end
+
+    def each
+      yield header
+
+      records.map do |record|
+        row = columns.map do |column|
+          column.block.call(record).to_s
+        end
+        yield row
+      end
     end
 
     private
 
-    def column key, value=nil, &block
-      @hash[key] = if block_given?
-        block.call(record)
-      elsif value.is_a?(Symbol)
-        record.send(value)
-      elsif value.nil?
-        record.send(key.parameterize.underscore.to_sym)
-      else
-        value
-      end.to_s
+    def header
+      columns.map(&:label)
     end
   end
 end
--- spec/decisive_spec.rb
+++ spec/decisive_spec.rb
@@ -3,74 +3,145 @@ RSpec.describe Decisive do
 
   Record = Struct.new(:a, :b, :c)
 
-  let(:response) { double(headers: {}) }
+  let(:response) { double(headers: {}, stream: double) }
   let(:controller) { double }
 
-  before do
-    @records = [
-      Record.new(1,2,3),
-      Record.new(4,5,6),
-      Record.new(7,8,9),
-    ]
-  end
+  context "#csv" do
+    before do
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+    end
 
-  it "works with a yielded record" do
-    template = Struct.new(:source).new <<~DECISIVE
-      csv @records, filename: "test.csv" do |record|
-        column "A"
-        column "Badgers", :b
-        column "C"
-        column "Sum" do
-          record.a + record.b + record.c
+    it "works with a yielded record" do
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do |record|
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "Sum" do
+            record.a + record.b + record.c
+          end
         end
-      end
-    DECISIVE
+      DECISIVE
 
-    result = eval(Decisive::TemplateHandler.call(template))
+      result = eval(Decisive::TemplateHandler.call(template))
 
-    expect(result).to eq <<~CSV
-      A,Badgers,C,Sum
-      1,2,3,6
-      4,5,6,15
-      7,8,9,24
-    CSV
+      expect(result).to eq <<~CSV
+        A,Badgers,C,Sum
+        1,2,3,6
+        4,5,6,15
+        7,8,9,24
+      CSV
 
-    expect(response.headers).to eq({
-      "Content-Disposition" => %(attachment; filename="test.csv"),
-      "Content-Type" => "text/csv",
-      "Content-Transfer-Encoding" => "binary",
-    })
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
+
+    it "works without a yielded record" do
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "D", "D"
+        end
+      DECISIVE
+
+      result = eval(Decisive::TemplateHandler.call(template))
+
+      expect(result).to eq <<~CSV
+        A,Badgers,C,D
+        1,2,3,D
+        4,5,6,D
+        7,8,9,D
+      CSV
+
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
   end
 
-  it "works without a yielded record" do
-    @records = [
-      Record.new(1,2,3),
-      Record.new(4,5,6),
-      Record.new(7,8,9),
-    ]
+  context "#stream_csv" do
+    before { allow(controller).to receive(:new_controller_thread).and_return(true) }
 
-    template = Struct.new(:source).new <<~DECISIVE
-      csv @records, filename: "test.csv" do
-        column "A"
-        column "Badgers", :b
-        column "C"
-        column "D", "D"
-      end
-    DECISIVE
+    it "works with a yielded record" do
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
 
-    result = eval(Decisive::TemplateHandler.call(template))
+      template = Struct.new(:source).new <<~DECISIVE
+        stream_csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "Sum" do |record|
+            record.a + record.b + record.c
+          end
+        end
+      DECISIVE
 
-    expect(result).to eq <<~CSV
-      A,Badgers,C,D
-      1,2,3,D
-      4,5,6,D
-      7,8,9,D
-    CSV
 
-    expect(response.headers).to eq({
-      "Content-Disposition" => %(attachment; filename="test.csv"),
-      "Content-Type" => "text/csv",
-      "Content-Transfer-Encoding" => "binary",
-    })
+      expect(response.stream).to receive(:write).with("A,Badgers,C,Sum\n")
+      expect(response.stream).to receive(:write).with("1,2,3,6\n")
+      expect(response.stream).to receive(:write).with("4,5,6,15\n")
+      expect(response.stream).to receive(:write).with("7,8,9,24\n")
+      expect(response.stream).to receive(:close)
+
+      eval(Decisive::TemplateHandler.call(template))
+
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
+
+    it "works without a yielded record" do
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        stream_csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column("D") { "D" }
+        end
+      DECISIVE
+
+      expect(response.stream).to receive(:write).with("A,Badgers,C,D\n")
+      expect(response.stream).to receive(:write).with("1,2,3,D\n")
+      expect(response.stream).to receive(:write).with("4,5,6,D\n")
+      expect(response.stream).to receive(:write).with("7,8,9,D\n")
+      expect(response.stream).to receive(:close)
+
+      eval(Decisive::TemplateHandler.call(template))
+
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
   end
 end
