25b7957847b4a8b85f9868c4b03b6d1ea6641cec
Micah Geisel <micah@botandrose.com>
2019-06-05 17:08:56 -0600

    merge stream_csv into csv and add guardrails.

--- lib/decisive/template_handler.rb
+++ lib/decisive/template_handler.rb
@@ -12,6 +12,7 @@ module Decisive
       <<~RUBY
         extend Decisive::DSL
         context = (#{template.source})
+
         response.headers["Content-Type"] = "text/csv"
         response.headers["Content-Transfer-Encoding"] = "binary"
         response.headers["Content-Disposition"] = %(attachment; filename="\#{context.filename}")
@@ -32,17 +33,63 @@ module Decisive
     end
   end
 
-  module DSL
-    def csv records, filename:, &block
-      Context.new(records, filename, block)
-    end
-
-    def stream_csv records, filename:, &block
-      StreamContext.new([], records, filename, &block)
+  class StreamIncompatibleBlockArgumentError < StandardError
+    def message
+      "#csv cannot take a block with a record argument while streaming, because the headers have to be known in advance. Either disable streaming by passing `stream: false` to #csv, or convert the template to yield the record to the block passed to each #column call."
     end
   end
 
-  class Context < Struct.new(:records, :filename, :block)
+  class StreamingNotEnabledByControllerError < StandardError
+    def message
+      "the controller does not have ActionController::Live included, and thus cannot stream this csv. Either disable streaming by passing `stream: false` to #csv, or include ActionController::Live into the controller."
+    end
+  end
+
+  module DSL
+    def csv records, filename:, stream: true, &block
+      if stream
+        raise StreamingNotEnabledByControllerError unless controller.respond_to?(:new_controller_thread) # has AC::Live mixed in
+        raise StreamIncompatibleBlockArgumentError if block.arity != 0
+        StreamContext.new([], records, filename, &block)
+      else
+        RenderContext.new(records, filename, block)
+      end
+    end
+  end
+
+  class StreamContext < Struct.new(:columns, :records, :filename)
+    class Column < Struct.new(:label, :block); end
+
+    def initialize *args, &block
+      super
+      instance_eval &block
+    end
+
+    def column label, value=nil, &block # field, label: field.to_s.humanize, &block
+      value ||= label.parameterize.underscore.to_sym
+      block ||= ->(record) { record.send(value) }
+      columns << Column.new(label, block)
+    end
+
+    def each
+      yield header
+
+      records.map do |record|
+        row = columns.map do |column|
+          column.block.call(record).to_s
+        end
+        yield row
+      end
+    end
+
+    private
+
+    def header
+      columns.map(&:label)
+    end
+  end
+
+  class RenderContext < Struct.new(:records, :filename, :block)
     def to_csv
       (header + body).map(&:to_csv).join
     end
@@ -89,38 +136,6 @@ module Decisive
           value
         end.to_s
       end
-    end
-  end
-
-  class StreamContext < Struct.new(:columns, :records, :filename)
-    class Column < Struct.new(:label, :block); end
-
-    def initialize *args, &block
-      super
-      instance_eval &block
-    end
-
-    def column label, value=nil, &block # field, label: field.to_s.humanize, &block
-      value ||= label.parameterize.underscore.to_sym
-      block ||= ->(record) { record.send(value) }
-      columns << Column.new(label, block)
-    end
-
-    def each
-      yield header
-
-      records.map do |record|
-        row = columns.map do |column|
-          column.block.call(record).to_s
-        end
-        yield row
-      end
-    end
-
-    private
-
-    def header
-      columns.map(&:label)
     end
   end
 end
--- spec/decisive_spec.rb
+++ spec/decisive_spec.rb
@@ -7,6 +7,119 @@ RSpec.describe Decisive do
   let(:controller) { double }
 
   context "#csv" do
+    it "works with yielding records to #column" do
+      allow(controller).to receive(:new_controller_thread).and_return(true)
+
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "Sum" do |record| record.a + record.b + record.c; end
+        end
+      DECISIVE
+
+
+      expect(response.stream).to receive(:write).with("A,Badgers,C,Sum\n")
+      expect(response.stream).to receive(:write).with("1,2,3,6\n")
+      expect(response.stream).to receive(:write).with("4,5,6,15\n")
+      expect(response.stream).to receive(:write).with("7,8,9,24\n")
+      expect(response.stream).to receive(:close)
+
+      eval(Decisive::TemplateHandler.call(template))
+
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
+
+    it "works without yielding records" do
+      allow(controller).to receive(:new_controller_thread).and_return(true)
+
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column("D") { "D" }
+        end
+      DECISIVE
+
+      expect(response.stream).to receive(:write).with("A,Badgers,C,D\n")
+      expect(response.stream).to receive(:write).with("1,2,3,D\n")
+      expect(response.stream).to receive(:write).with("4,5,6,D\n")
+      expect(response.stream).to receive(:write).with("7,8,9,D\n")
+      expect(response.stream).to receive(:close)
+
+      eval(Decisive::TemplateHandler.call(template))
+
+      expect(response.headers).to eq({
+        "Content-Disposition" => %(attachment; filename="test.csv"),
+        "Content-Type" => "text/csv",
+        "Content-Transfer-Encoding" => "binary",
+      })
+    end
+
+    it "raises an error when trying to yield a record to itself" do
+      allow(controller).to receive(:new_controller_thread).and_return(true)
+
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do |record|
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "Sum" do
+            record.a + record.b + record.c
+          end
+        end
+      DECISIVE
+
+      expect { eval(Decisive::TemplateHandler.call(template)) }.to raise_error(Decisive::StreamIncompatibleBlockArgumentError)
+    end
+
+    it "raises an error when trying to stream but controller doesnt support it" do
+      @records = [
+        Record.new(1,2,3),
+        Record.new(4,5,6),
+        Record.new(7,8,9),
+      ]
+
+      template = Struct.new(:source).new <<~DECISIVE
+        csv @records, filename: "test.csv" do
+          column "A"
+          column "Badgers", :b
+          column "C"
+          column "Sum" do |record|
+            record.a + record.b + record.c
+          end
+        end
+      DECISIVE
+
+      expect { eval(Decisive::TemplateHandler.call(template)) }.to raise_error(Decisive::StreamingNotEnabledByControllerError)
+    end
+  end
+
+  context "#csv stream: false" do
     before do
       @records = [
         Record.new(1,2,3),
@@ -15,9 +128,9 @@ RSpec.describe Decisive do
       ]
     end
 
-    it "works with a yielded record" do
+    it "works with yielding records to itself" do
       template = Struct.new(:source).new <<~DECISIVE
-        csv @records, filename: "test.csv" do |record|
+        csv @records, filename: "test.csv", stream: false do |record|
           column "A"
           column "Badgers", :b
           column "C"
@@ -43,7 +156,7 @@ RSpec.describe Decisive do
       })
     end
 
-    it "works without a yielded record" do
+    it "works without yielding records" do
       @records = [
         Record.new(1,2,3),
         Record.new(4,5,6),
@@ -51,7 +164,7 @@ RSpec.describe Decisive do
       ]
 
       template = Struct.new(:source).new <<~DECISIVE
-        csv @records, filename: "test.csv" do
+        csv @records, filename: "test.csv", stream: false do
           column "A"
           column "Badgers", :b
           column "C"
@@ -67,75 +180,6 @@ RSpec.describe Decisive do
         4,5,6,D
         7,8,9,D
       CSV
-
-      expect(response.headers).to eq({
-        "Content-Disposition" => %(attachment; filename="test.csv"),
-        "Content-Type" => "text/csv",
-        "Content-Transfer-Encoding" => "binary",
-      })
-    end
-  end
-
-  context "#stream_csv" do
-    before { allow(controller).to receive(:new_controller_thread).and_return(true) }
-
-    it "works with a yielded record" do
-      @records = [
-        Record.new(1,2,3),
-        Record.new(4,5,6),
-        Record.new(7,8,9),
-      ]
-
-      template = Struct.new(:source).new <<~DECISIVE
-        stream_csv @records, filename: "test.csv" do
-          column "A"
-          column "Badgers", :b
-          column "C"
-          column "Sum" do |record|
-            record.a + record.b + record.c
-          end
-        end
-      DECISIVE
-
-
-      expect(response.stream).to receive(:write).with("A,Badgers,C,Sum\n")
-      expect(response.stream).to receive(:write).with("1,2,3,6\n")
-      expect(response.stream).to receive(:write).with("4,5,6,15\n")
-      expect(response.stream).to receive(:write).with("7,8,9,24\n")
-      expect(response.stream).to receive(:close)
-
-      eval(Decisive::TemplateHandler.call(template))
-
-      expect(response.headers).to eq({
-        "Content-Disposition" => %(attachment; filename="test.csv"),
-        "Content-Type" => "text/csv",
-        "Content-Transfer-Encoding" => "binary",
-      })
-    end
-
-    it "works without a yielded record" do
-      @records = [
-        Record.new(1,2,3),
-        Record.new(4,5,6),
-        Record.new(7,8,9),
-      ]
-
-      template = Struct.new(:source).new <<~DECISIVE
-        stream_csv @records, filename: "test.csv" do
-          column "A"
-          column "Badgers", :b
-          column "C"
-          column("D") { "D" }
-        end
-      DECISIVE
-
-      expect(response.stream).to receive(:write).with("A,Badgers,C,D\n")
-      expect(response.stream).to receive(:write).with("1,2,3,D\n")
-      expect(response.stream).to receive(:write).with("4,5,6,D\n")
-      expect(response.stream).to receive(:write).with("7,8,9,D\n")
-      expect(response.stream).to receive(:close)
-
-      eval(Decisive::TemplateHandler.call(template))
 
       expect(response.headers).to eq({
         "Content-Disposition" => %(attachment; filename="test.csv"),
